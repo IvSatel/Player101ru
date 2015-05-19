e44a3cbac5fcd0f545ca6cb0a8792a5e1b4a5484
IvSatel <wincky@mail.ru>
2015-05-19 21:29:31 +0300

    Fix update

--- Main.py
+++ Main.py
@@ -3419,7 +3419,7 @@ def main_funck():
     remote_vers = ''
     with version_opener.open('https://raw.githubusercontent.com/IvSatel/Player101ru/master/version') as fo:
         remote_vers = fo.read().decode()
-    if SCRIPT_VERSION < remote_vers or (not os.path.exists(os.path.dirname(os.path.realpath(__file__)) +'/resource') or not os.path.isfile(os.path.exists(os.path.dirname(os.path.realpath(__file__)) +'/Radio.png'))):
+    if SCRIPT_VERSION < remote_vers or not os.path.exists(os.path.dirname(os.path.realpath(__file__)) +'/resource':
 
         download_up_date()
 
